046b597d60c89dc0a1cff422d3eb3ae3cf81318c
VladK <vlad.katrich1997@yandex.ru>
2025-06-11 14:59:33 +0300

    Исправленно

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -9,30 +9,25 @@ public interface TaskManager {
 
     List<Task> getAllTasks();
     void deleteAllTasks();
-
     Task getTask(int id);
     Task addTask(Task task);
-
     void updateTask(Task task);
     void deleteTask(int id);
 
-    List<Epic> getAllEpics();
 
+    List<Epic> getAllEpics();
     void deleteAllEpics();
     Epic getEpic(int id);
     Epic addEpic(Epic epic);
-
     void updateEpic(Epic epic);
     void deleteEpic(int id);
     List<Subtask> getEpicSubtasks(int epicId);
 
 
     List<Subtask> getAllSubtasks();
-
     void deleteAllSubtasks();
     Subtask getSubtask(int id);
     Subtask addSubtask(Subtask subtask);
-
     void updateSubtask(Subtask subtask);
     void deleteSubtask(int id);
 
