2c2fbf65e821e2067672af2fda7ee36d624ae1db
VladK <vlad.katrich1997@yandex.ru>
2025-07-11 22:25:18 +0300

    Исправлено

--- src/test/HttpTaskManagerTasksTest.java
+++ src/test/HttpTaskManagerTasksTest.java
@@ -2,73 +2,95 @@ package test;
 
 import com.google.gson.Gson;
 import http.HttpTaskServer;
-import model.Task;
 import model.TaskStatus;
+import model.Task;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import static http.HttpTaskServer.getGson;
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class HttpTaskManagerTasksTest {
-    private HttpTaskServer server;
-    private final Gson gson = HttpTaskServer.getGson();
-    private final HttpClient client = HttpClient.newHttpClient();
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class HttpTaskManagerTasksTest {
+    private static HttpTaskServer server;
+    private static HttpClient client;
+    private static final Gson gson = getGson();
 
     @BeforeAll
-    void startServer() throws IOException {
+    static void setup() throws IOException {
         server = new HttpTaskServer();
         server.start();
+        client = HttpClient.newHttpClient();
     }
 
     @AfterAll
-    void stopServer() {
+    static void teardown() {
         server.stop();
     }
 
-    @BeforeEach
-    void clearTasks() throws IOException, InterruptedException {
-        HttpRequest deleteAllRequest = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/tasks/task/"))
-                .DELETE()
-                .build();
-        HttpResponse<String> deleteResponse = client.send(deleteAllRequest, HttpResponse.BodyHandlers.ofString());
-        assertTrue(deleteResponse.statusCode() == 200 || deleteResponse.statusCode() == 204);
+    @Test
+    @Order(1)
+    void shouldAddTask() throws IOException, InterruptedException {
+        Task task = new Task(0, "Test Task", "Description", TaskStatus.NEW,
+                Duration.ofMinutes(30), LocalDateTime.now());
+
+        HttpResponse<String> response = postTask(task);
+        assertEquals(201, response.statusCode());
+
+        Task created = gson.fromJson(response.body(), Task.class);
+        assertEquals(task.getName(), created.getName());
     }
 
     @Test
-    void shouldAddAndGetTask() throws IOException, InterruptedException {
-        Task task = new Task(1, "Test Task", "Description", TaskStatus.NEW,
-                Duration.ofMinutes(30), LocalDateTime.now());
+    @Order(2)
+    void shouldGetTaskById() throws IOException, InterruptedException {
+        Task task = new Task(0, "Another Task", "Desc", TaskStatus.NEW,
+                Duration.ofMinutes(15), LocalDateTime.now());
 
-        HttpRequest postRequest = HttpRequest.newBuilder()
+        Task created = gson.fromJson(postTask(task).body(), Task.class);
+        HttpResponse<String> response = getTaskById(created.getId());
+
+        assertEquals(200, response.statusCode());
+        Task fetched = gson.fromJson(response.body(), Task.class);
+        assertEquals(created, fetched);
+    }
+
+    @Test
+    @Order(3)
+    void shouldDeleteAllTasks() throws IOException, InterruptedException {
+        HttpResponse<String> response = deleteAllTasks();
+        assertEquals(200, response.statusCode());
+    }
+
+    private HttpResponse<String> postTask(Task task) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:8080/tasks/task/"))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(task)))
                 .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
 
-        HttpResponse<String> postResponse = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
-        assertEquals(201, postResponse.statusCode());
-
-        HttpRequest getRequest = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/tasks/task/?id=1"))
+    private HttpResponse<String> getTaskById(int id) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/tasks/task/?id=" + id))
                 .GET()
                 .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
 
-        HttpResponse<String> getResponse = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
-        assertEquals(200, getResponse.statusCode());
-
-        Task retrieved = gson.fromJson(getResponse.body(), Task.class);
-        assertNotNull(retrieved);
-        assertEquals(task.getName(), retrieved.getName());
-        assertEquals(task.getDescription(), retrieved.getDescription());
-        assertEquals(task.getStatus(), retrieved.getStatus());
-        assertEquals(task.getDuration(), retrieved.getDuration());
-        assertEquals(task.getStartTime(), retrieved.getStartTime());
+    private HttpResponse<String> deleteAllTasks() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/tasks/task/"))
+                .DELETE()
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
